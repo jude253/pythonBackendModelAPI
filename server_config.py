bebd592126e2cdd2e4222e12fa65fe4213bcaae4
jude253 <judecapachietti@gmail.com>
2020-12-28 15:28:32 -0500

    -added requirements.txt with all dependencies

--- server_config.py
+++ server_config.py
@@ -3,4 +3,4 @@ from route_config import *
 app.debug = True
 host = os.environ.get('IP', '0.0.0.0')
 port = int(os.environ.get('PORT', 8080))
-app.run(host=host, port=port)
+app.run(host=host, debug=False)
