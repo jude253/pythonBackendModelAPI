bfbe4d55834677ef229bd74ab88c2f74b3e5195c
jude253 <judecapachietti@gmail.com>
2021-01-04 10:27:49 -0500

    -renaming app.py and using server_config.py

--- server_config.py
+++ server_config.py
@@ -1,9 +1,8 @@
-#I am not sure that this file is necessary or that it helps
+# I am not sure that this file is necessary or that it helps
 
-# import os
-# from app import *
-# app.debug = True
-# host = os.environ.get('IP', '0.0.0.0')
-# port = int(os.environ.get('PORT', 8080))
-# app.run()
-# app.run(host=host,port=port, debug=False)
+import os
+from route_config import *
+app.debug = True
+host = os.environ.get('IP', '0.0.0.0')
+port = int(os.environ.get('PORT', 8080))
+app.run(host='0.0.0.0',port=8080)
