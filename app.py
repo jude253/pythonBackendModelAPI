dfcc9acc868da854c95d7ae6247031ff4640ab46
jude253 <judecapachietti@gmail.com>
2021-02-12 18:18:44 -0500

    -added app.py and wsgi.py from the production server -because I am going to wipe the server and re-pull from git

---
+++ app.py
@@ -0,0 +1,11 @@
+
+# I am not sure that this file is necessary or that it helps
+# this is from production.. not sure if needed
+import os
+from route_config import *
+app.debug = True
+host = os.environ.get('IP', '0.0.0.0')
+port = int(os.environ.get('PORT', 8080))
+
+if __name__ == "__main__":
+    app.run(host=host)
